e3cbee068bbe05b48a7c46d5c9dfca5a18bb6ff1
Brisstone <okolijohnson69@gmail.com>
2024-05-30 00:54:57 +0100

    OJ-service-tests

---
+++ app/Http/Services/CustomerService.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Service;
+
+use Illuminate\Support\Facades\Validator;
+
+class CustomerService
+{
+
+    /**
+     * Create a new process flow.
+     *
+     * @param  array  $data
+     * @return object
+     */
+    public function createCustomerJob(array $data): object
+    {
+        $validator = Validator::make($data, [
+            "name" => "required|string|max:255",
+        ]);
+
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+        //TODO:: KEEP RECORD OF PROCESS
+        return (object)array($data);
+    }
+}
